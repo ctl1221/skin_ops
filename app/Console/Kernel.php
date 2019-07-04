31f099fa704181ad42963591cd7b54a8e9ca4c4c
Charles Stephen Licup <ctl1221@gmail.com>
2019-07-04 14:06:32 +0800

    Commit 106

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,10 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
+        $date = \Carbon\Carbon::now()->toDateString();
         $schedule->command('membership:check')->everyMinute();   
-        $schedule->command('report:daily_sales')->dailyAt('22:30'); 
+        //$schedule->command('report:daily_sales ' . $date)->dailyAt('22:30');
+        $schedule->command('report:daily_sales ' . $date)->everyMinute(); 
 
     }
 
