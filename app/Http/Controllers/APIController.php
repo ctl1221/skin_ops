aa3485a1d290b5aa43c3372bf7434215ae04d6c4
Charles Stephen Licup <ctl1221@gmail.com>
2019-07-04 12:07:00 +0800

    Commit 97

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -136,11 +136,11 @@ class APIController extends Controller
 			$sales_orders
 				->where(function ($query) use ($request) {
 	                $query->where('so_number','like','%' . $request->filter . '%')
-					$query->orWhere('date','like','%' . $request->filter . '%')
-					$query->orWhereHas('client', function ($q) use ($request) {
+					->orWhere('date','like','%' . $request->filter . '%')
+					->orWhereHas('client', function ($q) use ($request) {
 						$q->where('first_name','like','%' . $request->filter . '%');
 					})
-					$query->orWhereHas('client', function ($q) use ($request) {
+					->orWhereHas('client', function ($q) use ($request) {
 						$q->where('last_name','like','%' . $request->filter . '%');
 					});
 	            });
